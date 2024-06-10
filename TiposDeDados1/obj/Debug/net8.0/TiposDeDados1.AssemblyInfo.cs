bc9f1438bb50da8d5181870c662b857f713e48ec
therealuiiz <fluiz4216@gmail.com>
2024-06-10 12:19:35 -0300

    solucaoproblemas

--- TiposDeDados1/obj/Debug/net8.0/TiposDeDados1.AssemblyInfo.cs
+++ TiposDeDados1/obj/Debug/net8.0/TiposDeDados1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiposDeDados1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f080167ddf2ff047934258fc78f9c6da868566e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d0079ed401d96804f93c16c42de55468e9f7de5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiposDeDados1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiposDeDados1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
